91758eec8d084cfa597f5234ed1feb21ffe3d4c1
bryan arroyo guzman <bryan@grupomundial.cr>
2020-12-16 11:20:46 -0600

    test

--- ClientesWebAngular/Startup.cs
+++ ClientesWebAngular/Startup.cs
@@ -13,7 +13,6 @@ namespace ClientesWebAngular
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-
         }
 
         public IConfiguration Configuration { get; }
